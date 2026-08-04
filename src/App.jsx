202a5369d93c3fb37e651584ffaeda4c3ca7fdf5
Offichigo <oph.perianin@gmail.com>
2026-08-04 11:23:06 +0200

    Initial commit: setup Vite + React + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import TitleScreen from "./components/TitleScreen";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-[#F7F0F5] flex items-center justify-center p-6">
+      <TitleScreen />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TitleScreen.jsx
@@ -0,0 +1,6 @@
+function TitleScreen() {
+  return (
+  )
+}
+
+export default TitleScreen;
