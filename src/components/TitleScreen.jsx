00818aa12cf991a9425bfa175a38ab272ae95b5b
Offichigo <oph.perianin@gmail.com>
2026-08-04 13:49:49 +0200

    feature: jsx title screen

--- src/components/TitleScreen.jsx
+++ src/components/TitleScreen.jsx
@@ -1,6 +1,17 @@
 function TitleScreen() {
   return (
-  )
+    <div>
+      <div>
+        <span>untitled — myPortfolio.exe</span>
+      </div>
+      <div>
+        <span>disponible stage 21 sept au 30 oct 2026</span>
+        <h1>Ophélie (Off) Perianin</h1>
+        <p>Développeuse web full stack</p>
+        <button>Start →</button>
+      </div>
+    </div>
+  );
 }
 
 export default TitleScreen;
